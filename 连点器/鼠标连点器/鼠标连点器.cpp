b1f0f9c502d7019b3961703fd06e8a0115e1c1f5
RWGLQJ <79353322+RWGLQJ@users.noreply.github.com>
2021-09-18 14:23:19 +0800

    Add files via upload

---
+++ 连点器/鼠标连点器/鼠标连点器.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    double times;
+    cout << "按空格键开始连点，ESC键退出" << endl;
+    cout << "请输入连点距离：";
+    cin >> times;
+    while (true)
+    {
+        if (typeid(times) != typeid(double))
+        {
+            cout << "请输入连点距离：";
+            cin >> times;
+        }
+        else { break; }
+    }
+    while (true)
+    {
+        if (GetAsyncKeyState(VK_SPACE))
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            Sleep(times);
+            if (GetAsyncKeyState(VK_ESCAPE))break;
+        }
+    }
+    return 0;
+}
